ddd87ad01052c697d4180956959f2f3898642ef5
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-23 22:08:29 +0100

    Update RazzoSpaziale.java
    Scritto costruttore vuoto della classe RazzoSpaziale

--- RazzoSpaziale.java
+++ RazzoSpaziale.java
@@ -3,5 +3,10 @@ import java.util.ArrayList;
 public class RazzoSpaziale extends Razzo{
     //attributo
     private ArrayList<String> oggettiContenuti;
-    
+
+    //costruttore
+    public RazzoSpaziale(){
+        super();
+        this.oggettiContenuti = new ArrayList<String>();
+    }
 }
