42bb047e4063c708d0a191d89b0959b8ac897d82
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-23 21:17:15 +0100

    Update Razzo.java
    Creato costruttore vuoto della classe Razzo

--- Razzo.java
+++ Razzo.java
@@ -6,5 +6,12 @@ public class Razzo {
     protected int numeroMotori;
     protected String id;
 
+    //costruttori
+    public Razzo(){
+        this.nomeRazzo = "";
+        this.raggio = 0;
+        this.numeroMotori = 0;
+        this.id = "";
+    }
     
 }
