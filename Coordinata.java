918e833078988b8fbad5fe4c3a13888699b60634
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-23 22:23:04 +0100

    Update Coordinata.java
    creato costruttore vuoto della classe Coordinata

--- Coordinata.java
+++ Coordinata.java
@@ -2,4 +2,10 @@ public class Coordinata {
     //attributi
     private double x;
     private double y;
+
+    //costruttori
+    public Coordinata(){
+        this.x = 0;
+        this.y = 0;
+    }
 }
