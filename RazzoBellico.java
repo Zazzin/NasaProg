80a5d9b35a3a461f8b08a1ad2ac63c15495bca20
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-23 22:40:08 +0100

    Update RazzoBellico.java
    creato costruttore che assegna dei valori non nulli agli attributi di RazzoBellico

--- RazzoBellico.java
+++ RazzoBellico.java
@@ -1,8 +1,16 @@
-public class RazzoBellico {
+import java.util.ArrayList;
+
+public class RazzoBellico extends Razzo{
     //attributi
     Coordinata coordinate;
 
     public RazzoBellico(){
+        super();
         this.coordinate = new Coordinata();
+    }
+
+    public RazzoBellico(String nomeRazzo,double raggio, int numeroMotori, String id, double x, double y){
+        super(nomeRazzo, raggio, numeroMotori, id);
+        coordinate = new Coordinata(x, y);        
     }
 }
