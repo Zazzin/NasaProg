af8dc879f4779f85348f7be0b48a223091be5851
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-24 12:42:17 +0100

    Update Razzi.java
    creato costruttore della classe Razzi

--- Razzi.java
+++ Razzi.java
@@ -3,4 +3,9 @@ import java.util.ArrayList;
 public class Razzi {
     //attributi
     ArrayList<Razzo> insiemeRazzi;
+
+    //costruttore
+    public Razzi(){
+        this.insiemeRazzi = new ArrayList<Razzo>();
+    }
 }
