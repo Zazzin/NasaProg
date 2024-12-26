08707c4c3e14e363a0ecaa29a9ba5e8b59f02ace
lrztnc <lorenzo.tincani@icloud.com>
2024-12-26 21:10:10 +0100

    Menus

--- GesioneSistema.java
+++ GesioneSistema.java
@@ -2,6 +2,8 @@ public class GesioneSistema {
     Razzi razzo;
     Lavoratori lavoratore;
     Amministratore amministratore;
+    private boolean utenteAutenticato = false;
+
     // log
     public GesioneSistema() {
         razzo = new Razzi();
@@ -27,9 +29,9 @@ public class GesioneSistema {
 
     public void login(Lavoratore lavoratore, Amministratore amministratore, String inputUsername, String inputPassword) {
         if (lavoratore.getNomeUtente().equals(inputUsername) && lavoratore.getPassword().equals(inputPassword)) {
-
+            utenteAutenticato = true;
         } else if (amministratore.getNomeUtente().equals(inputUsername) && amministratore.getPassword().equals(inputPassword)) {
-
+            
         }
             
     }
@@ -37,6 +39,19 @@ public class GesioneSistema {
     public void logout() {
         this.amministratore = null;
         this.lavoratore = null;
+    }
+
+    public void MenuLavoratore() {
+        lavoratore.addLavoratore(amministratore);
+        
+        //
+        //
+    }
+
+    public void MenuAmministratore() {
+        amministratore.getUtenti(null);
+        amministratore.printUtenti(null);
+        amministratore.toString();
     }
 
 
