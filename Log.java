fcfa74019f8e211ef9d69519109b6917ad4ec0b3
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-26 20:02:18 +0100

    Update Log.java
    Scritto costruttore della classe Log

--- Log.java
+++ Log.java
@@ -3,4 +3,10 @@ import java.util.ArrayList;
 public class Log {
     //contenitore delle azioni con la data e l'ora
     ArrayList<Azione> azioni;
+    
+    //costruttore
+    public Log(){
+        this.azioni = new ArrayList<>();
+    }
+
 }
