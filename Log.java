fcde8af8b0d39dcb0b7a216af7146623e85cc544
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-26 20:03:45 +0100

    Update Log.java
    Scritto getter della classe Log

--- Log.java
+++ Log.java
@@ -3,10 +3,14 @@ import java.util.ArrayList;
 public class Log {
     //contenitore delle azioni con la data e l'ora
     ArrayList<Azione> azioni;
-    
+
     //costruttore
     public Log(){
         this.azioni = new ArrayList<>();
     }
 
+    //getter
+    public ArrayList<Azione> getAzioni(){
+        return this.azioni;
+    }
 }
