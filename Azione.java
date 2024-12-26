09e7dc23089cc559a6c34d4017bc21608b496964
Y4ss1nB <168534030+Y4ss1nB@users.noreply.github.com>
2024-12-26 19:45:20 +0100

    Update Azione.java
    Aggiunti due costruttori alla classe Azione

--- Azione.java
+++ Azione.java
@@ -4,4 +4,17 @@ public class Azione {
      //attributi
      private String azione;
      private Calendar dataEOra;
+
+    //costruttori
+    public Azione(){
+        this.azione = " ";
+        this.dataEOra = Calendar.getInstance();
+    }
+
+    public Azione(String azione){
+        this.azione = azione;
+        this.dataEOra = Calendar.getInstance();
+    }
+
+    
 }
